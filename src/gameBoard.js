99521ac6082d137331f24c171f889c4afb5ef316
Jordanjhiggins <jordan.higgins14@gmail.com>
2023-04-12 05:29:52 -0300

    add game board factory skeleton

---
+++ src/gameBoard.js
@@ -0,0 +1,15 @@
+import Ship from './ship';
+
+const GameBoard = () => {
+  const boardSize = 10;
+  let missedAttack = 0;
+
+  const createBoard = () => {};
+  const placeShip = () => {};
+  const receiveAttack = () => {};
+  const fleetSunk = () => {};
+
+  return { placeShip, receiveAttack, fleetSunk };
+};
+
+export default GameBoard;
