264f035fad3544d994756d157c8d82197eb29332
Jordanjhiggins <jordan.higgins14@gmail.com>
2023-04-24 18:51:24 -0300

    add attack methods to player factory

--- src/player.js
+++ src/player.js
@@ -12,28 +12,42 @@ const Player = (name) => {
     destroyer: Ship('destroyer', 2),
   };
 
-  const attack = (x, y, gameBoard) => {
-    // if (checkMove) return
-
-    gameBoard.receiveAttack();
-  };
-
-  const randomAttack = () => {
-    // if (checkMove return
-  };
-
   const checkMove = (x, y, board) => {
     if (
       board.beenHit.forEach((coordPair) => {
         if (coordPair[0] === x && coordPair[1] === y) {
-          return 'This space has already been hit';
+          return true;
         }
       })
     )
-      return 'this space is Free';
+      return false;
   };
 
-  return { gameBoard, fleet, playerName, checkMove };
+  const attack = (x, y, board) => {
+    if (checkMove(x, y, board)) gameBoard.receiveAttack(x, y);
+  };
+
+  const randomAttack = (board) => {
+    const randomX = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+    const randomY = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+
+    if (checkMove(randomX, randomY, board) !== true) {
+      return board.receiveAttack(randomX, randomY);
+    }
+    console.log('bingbong');
+  };
+
+  return { gameBoard, fleet, playerName, checkMove, attack, randomAttack };
 };
 
 const player1 = Player('player1');
+
+// LOGIC TESTING
+console.log(player1.gameBoard.createBoard());
+console.log(
+  player1.gameBoard.placeShip(player1.fleet.carrier, 'horizontal', 2, 4)
+);
+console.log(player1.gameBoard.receiveAttack(2, 4));
+// console.log(player1.gameBoard.beenHit);
+// console.log(player1.checkMove(2, 4, player1.gameBoard));
+console.log(player1.randomAttack(player1.gameBoard));
