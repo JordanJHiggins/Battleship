4a8879c269b6ce83a139084400c947117015cf77
Jordanjhiggins <jordan.higgins14@gmail.com>
2023-05-02 20:27:59 -0300

    add method to set data-attribute of grid elements to corresponsing array index

--- src/view.js
+++ src/view.js
@@ -5,9 +5,15 @@ const renderBoard = () => {
   const gridTwo = document.getElementById('grid-two');
 
   for (let i = 0; i < 100; i++) {
+    const row = parseInt(i / 10, 10);
+    const col = i % 10;
+
     const cell = document.createElement('div');
     cell.classList.add('cell');
     cell.setAttribute('id', `player-position-${i}`);
+
+    cell.setAttribute('data-X-coordinate', `${row}`);
+    cell.setAttribute('data-Y-coordinate', `${col}`);
 
     gridOne.append(cell);
   }
