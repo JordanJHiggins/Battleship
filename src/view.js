ae5906df49bf97a42c829f3fe32c34dd2d3aa768
Jordanjhiggins <jordan.higgins14@gmail.com>
2023-05-05 13:09:44 -0300

    add method to render render ships

--- src/view.js
+++ src/view.js
@@ -1,22 +1,41 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
+const View = (playerName) => {
+  // const playerGrid = document.getElementById('player-grid');
+  // const computerGrid = document.getElementById('computer-grid');
 
-const renderBoard = () => {
-  const gridOne = document.getElementById('grid-one');
-  const gridTwo = document.getElementById('grid-two');
+  const renderBoard = (grid) => {
+    const currentGrid = document.getElementById(grid);
 
-  for (let i = 0; i < 100; i++) {
-    const row = parseInt(i / 10, 10);
-    const col = i % 10;
+    for (let i = 0; i < 100; i++) {
+      const row = parseInt(i / 10, 10);
+      const col = i % 10;
 
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.setAttribute('id', `player-position-${i}`);
+      const cell = document.createElement('div');
+      cell.classList.add(`cell`);
+      cell.setAttribute('id', `player-cell-${i}`);
+      cell.setAttribute('data-X-coordinate', `${row}`);
+      cell.setAttribute('data-Y-coordinate', `${col}`);
 
-    cell.setAttribute('data-X-coordinate', `${row}`);
-    cell.setAttribute('data-Y-coordinate', `${col}`);
+      currentGrid.append(cell);
+    }
+  };
+  // How to select adjacent tiles, increment Y coord?
+  const renderShip = (ship) => {
+    const currentGrid = document.querySelector('#player-grid');
+    currentGrid.addEventListener('click', (e) => {
+      e.target.classList.add('attack');
+      const siblings = e.target.parentElement.children;
+      const index = Array.from(siblings).indexOf(e.target);
+      for (
+        let i = index + 1;
+        i < index + ship.shipLength && i < siblings.length;
+        i++
+      ) {
+        siblings[i].classList.add('attack');
+      }
+    });
+  };
 
-    gridOne.append(cell);
-  }
+  return { renderBoard, renderShip };
 };
-
-export default renderBoard;
+export default View;
