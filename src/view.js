d1128124286fea0a3e971e82f0de3e54aa61522e
Jordanjhiggins <jordan.higgins14@gmail.com>
2023-05-06 12:27:12 -0300

    add method to check for placements out of range

--- src/view.js
+++ src/view.js
@@ -3,6 +3,26 @@ const View = (playerName) => {
   const playerGrid = document.getElementById('player-grid');
   // const computerGrid = document.getElementById('computer-grid');
 
+  const validatePlacement = (event, ship) => {
+    const siblings = event.target.parentElement.children;
+    const index = Array.from(siblings).indexOf(event.target);
+
+    // Check if cells in ships placement range already have 'attack' class.
+    for (
+      let i = index;
+      i < index + ship.shipLength && i < siblings.length;
+      i++
+    ) {
+      if (siblings[i].classList.contains('attack')) return false;
+    }
+
+    // Check if the last cell in ships placement range wraps to next row.
+    const lastCell = siblings[index + ship.shipLength - 1];
+    if (lastCell.getAttribute('data-y-coordinate') === '0') return false;
+
+    return true;
+  };
+
   const renderBoard = (grid) => {
     const currentGrid = document.getElementById(grid);
 
@@ -13,8 +33,8 @@ const View = (playerName) => {
       const cell = document.createElement('div');
       cell.classList.add(`cell`);
       cell.setAttribute('id', `player-cell-${i}`);
-      cell.setAttribute('data-X-coordinate', `${row}`);
-      cell.setAttribute('data-Y-coordinate', `${col}`);
+      cell.setAttribute('data-x-coordinate', `${row}`);
+      cell.setAttribute('data-y-coordinate', `${col}`);
 
       currentGrid.append(cell);
     }
@@ -23,12 +43,13 @@ const View = (playerName) => {
   const renderShip = (ship) => {
     const currentGrid = document.querySelector('#player-grid');
 
-    currentGrid.addEventListener('click', (e) => {
-      if (e.target.classList.contains('attack')) return;
-      e.target.classList.add('attack');
+    currentGrid.addEventListener('click', (event) => {
+      if (validatePlacement(event, ship) === false) return;
 
-      const siblings = e.target.parentElement.children;
-      const index = Array.from(siblings).indexOf(e.target);
+      event.target.classList.add('attack');
+
+      const siblings = event.target.parentElement.children;
+      const index = Array.from(siblings).indexOf(event.target);
 
       for (
         let i = index + 1;
